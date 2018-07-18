ed9719215a1470d8cfbe6070395595c083eaa08c
Reza Fachry <reza.fachry.a@gmail.com>
2018-07-18 14:48:27 +0700

    Mobile-menu modularity

---
+++ aphrodite/inc/mobile-menu.php
@@ -0,0 +1,13 @@
+<ul>
+  <li class="page-scroll"><a href="#salon">The Salon</a></li>
+  <li class="page-scroll"><a href="#ourteam">Meet our team</a></li>
+  <li class="page-scroll"><a href="#services">Our Services</a></li>
+  <li class="page-scroll"><a href="#gallery">portfolio</a></li>
+  <li class="page-scroll"><a href="#promotions">Promotions</a></li>
+  <li class="page-scroll"><a href="#video">Video</a></li>
+  <li><a href="blog.html">Blog</a></li>
+  <li><a href="blog-classic.html">Blog Classic</a></li>
+  <li><a href="blog-fullwidth.html">Blog Full Width</a></li>
+  <li><a href="blog-masonary.html">Blog Masonary</a></li>
+  <li class="page-scroll"><a href="#contact">Contact</a></li>
+</ul>
