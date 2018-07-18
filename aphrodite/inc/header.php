dbea2da6e8d76e633b0f01bf4191e5f9025a6953
Reza Fachry <reza.fachry.a@gmail.com>
2018-07-18 14:55:13 +0700

    Update blog nav & href

--- aphrodite/inc/header.php
+++ aphrodite/inc/header.php
@@ -12,15 +12,9 @@
               <li class="page-scroll"><a href="#services">Our Services</a></li>
               <li class="page-scroll"><a href="#gallery">portfolio</a></li>
               <li class="page-scroll"><a href="#promotions">Promotions</a></li>
-             	<li><a href="blog.html">Blog</a>
-              		<ul>
-                        <li><a href="blog-classic.html">Blog Classic</a></li>
-                        <li><a href="blog-fullwidth.html">Blog Full Width</a></li>
-                        <li><a href="blog-masonary.html">Blog Masonary</a></li>
-                    </ul>
-              </li>
-               <li class="page-scroll"><a href="#contact">Contact</a></li>
-               <li class="page-scroll" style="background-color: #292929;"><a href="#contact" style="color: #fff;">Make an Appointment</a></li>
+             	<li class="page-scroll"><a href="blog.html">Blog</a></li>
+              <li class="page-scroll"><a href="#contact">Contact</a></li>
+              <li class="page-scroll" style="background-color: #292929;"><a href="#contact" style="color: #fff;">Make an Appointment</a></li>
             </ul>
           </div>
         </div>
--- aphrodite/inc/mobile-menu.php
+++ aphrodite/inc/mobile-menu.php
@@ -1,13 +1,10 @@
 <ul>
-  <li class="page-scroll"><a href="#salon">The Salon</a></li>
-  <li class="page-scroll"><a href="#ourteam">Meet our team</a></li>
+  <li class="page-scroll"><a href="index.php">Home</a></li>
+  <li class="page-scroll"><a href="#ourteam">Eyelash Types</a></li>
   <li class="page-scroll"><a href="#services">Our Services</a></li>
   <li class="page-scroll"><a href="#gallery">portfolio</a></li>
   <li class="page-scroll"><a href="#promotions">Promotions</a></li>
-  <li class="page-scroll"><a href="#video">Video</a></li>
-  <li><a href="blog.html">Blog</a></li>
-  <li><a href="blog-classic.html">Blog Classic</a></li>
-  <li><a href="blog-fullwidth.html">Blog Full Width</a></li>
-  <li><a href="blog-masonary.html">Blog Masonary</a></li>
+  <li class="page-scroll"><a href="blog.html">Blog</a></li>
   <li class="page-scroll"><a href="#contact">Contact</a></li>
+  <li class="page-scroll" style="background-color: #292929;"><a href="#contact" style="color: #fff;">Make an Appointment</a></li>
 </ul>
